f3da3a4dcc450ae85d26a634edcefd495976d851
Augustus Franklin <augustus@gaglers.com>
2015-05-04 13:31:22 +0000

    Added 2 more utilities

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 callhub-examples
 ================
 
-CallHub, https://callhub.io/ helps businesses get qualified live leads in a matter of minutes. CallHub broadcasts interactive voice messages to thousands of people and connects interested people directly to the sales teams.
+CallHub, https://callhub.io/ helps businesses setup their complete telephony infrastructure in a matter of minutes. CallHub broadcasts interactive voice messages to thousands of people and connects interested people directly to the sales teams.
 
 Voice Broadcasting and automated phone dialers have existed for a long time. People used to use a voice modem  and buy software from automatic dialer providers for their computers. These systems have an upfront cost and are limited by the number of telephone connections they had. It also tied up their telephone lines from other uses. The most expensive and higher end of these automated dialer software maxed out at about 75-100 simultaneous calls and cost anywhere between $20,000 - $50,000. Plus, you still had to pay for the calls that you made.
 
 By utilising the elasticity of the cloud and the recent advances in communication software, we can now scale and automate these traditional business applications to much greater magnitude and at a greatly reduced cost. For instance, CallHub can make 6000 simultaneous calls at a cost of 1.5c for a 30 sec call. With this we can make voice broadcasting available to every all businesses for as low as $25. This turns the traditional phone dialer and voice broadcasting business on its head. This is like the shift from the traditional on-site web hosting to the cloud-based pay-as-you-go hosting model for voice broadcasting industry.
-
-The complete features of CallHub is here, https://callhub.io/voice-broadcasting/
 
 For any sales and marketing organisation, the ability to generate live leads that they can talk to is invaluable. Only large businesses could afford these phone dialer systems before. CallHub makes it these highly scalable and reliable systems available to all businesses from small one-man shops to large corporations.
 
---
+++ utilities/addtag.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+#
+# The Initial Developer of the Original Code is
+# Jadon <jadon@gaglers.com>
+#
+import requests, time, sys, json
+
+def get_people_ids(list_args, log_file):
+    to_delete = []
+    start = end = 0
+    for arg in list_args:
+        split_arr = arg.split(':')
+        split_len = len(split_arr)
+        if split_len == 2:
+            try:
+                start = int(split_arr[0])
+                end = int(split_arr[1]) + 1
+                for i in range(start, end):
+                    to_delete.append(i)
+            except:
+                log_file.write("Invalid Range given: '%s'\n" % arg)
+        elif split_len == 1:
+            try:
+                list_id = int(arg)
+                to_delete.append(arg)
+            except:
+                log_file.write("Invalid list ID: '%s' not a number\n" % arg)
+        log_file.flush()
+    return to_delete
+
+def main(argv):
+    if len(argv) < 4:
+        print "Usage: %s <slug> <access_token> <tag> <person_id> <person_id>" % argv[0]
+        print "OR" 
+        print "Usage: %s <slug> <access_token> <tag> <start:end>" % argv[0]
+        return
+
+    output = open('/tmp/addtag.log', 'a')
+    SLUG = argv[1]
+    ACCESS_TOKEN= argv[2]
+    tag_arg = argv[3]
+    tags = tag_arg.split(',')
+    people_ids = get_people_ids(argv[4:], output)
+    log_text = ''
+    for i in people_ids:
+        try:
+            url = "https://%s.nationbuilder.com/api/v1/people/%s/taggings?access_token=%s" % \
+                (SLUG, i, ACCESS_TOKEN)
+            tag_body = {}
+            tag_body["tagging"] = { 'tag': tags }
+            r = requests.put(url, data=json.dumps(tag_body),
+                            headers={'content-type': 'application/json',
+                                     'accept':'application/json'})
+            if r.status_code == 200:
+                log_text = "Added Tag to %s at %s.nationbuilder.com\n" % (i, SLUG)
+            else:
+                log_text = "Tagging Failed for %s at %s.nationbuilder.com,"\
+                    " error code:%s. Try again later.\n" % (i, SLUG, r.status_code)
+        except Exception as e:
+            log_text = "Exceptions for url:%s Error:%s\n" % (url, str(e))
+            pass
+        output.write(log_text)
+        print log_text
+        output.flush()
+        time.sleep(1)
+    log_text = "\n\n"
+    output.write(log_text)
+    output.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
